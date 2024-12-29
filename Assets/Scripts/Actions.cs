f659c9c4c0e18db1157eb846eb7c0d132b1c7ceb
olesyaVoz <odv1@tpu.ru>
2024-12-29 18:43:54 +0700

    Update Actions

--- Assets/Scripts/Actions.cs
+++ Assets/Scripts/Actions.cs
@@ -21,6 +21,11 @@ public class Actions : MonoBehaviour
             startPosX = mousePos.x - this.transform.localPosition.x;
             startPosY = mousePos.y - this.transform.localPosition.y;
 
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.sortingOrder = 10; 
+            }
         }
     }
 
@@ -34,7 +39,7 @@ public class Actions : MonoBehaviour
             {
                 WinScript.AddElement();
                 SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-                this.transform.position = new Vector2(form.transform.position.x, transform.position.y);
+                this.transform.position = form.transform.position;
                 if (spriteRenderer != null)
                 {
                     spriteRenderer.sortingOrder = 1;
@@ -43,10 +48,19 @@ public class Actions : MonoBehaviour
                 GetComponent<BoxCollider2D>().enabled = false;
             }
         }
+        else
+        {
+           
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null && !placed)
+            {
+                spriteRenderer.sortingOrder = 5; 
+            }
+        }
     }
     void Update()
     {
-        if (move == true)
+        if (move)
         {
             mousePos = Input.mousePosition;
             this.gameObject.transform.localPosition = new Vector2(mousePos.x - startPosX, mousePos.y - startPosY);
